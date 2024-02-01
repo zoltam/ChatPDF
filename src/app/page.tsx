005b8d83b6226f765b400347e4d157f066def4e4
zoltam <zoltan.ruutuvaara@gmail.com>
2024-02-01 13:50:01 +0200

    add existing files to git

---
+++ src/app/page.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
+import {LogIn} from "lucide-react"
+
+export default async function Home() {
+  const { userId } = await auth()
+  const isAuth = !!userId
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton  afterSignOutUrl="/"></UserButton>  
+          </div>
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+            
+          </div>
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students, researchers and proffessionals to instantly answer questions and understand research with Ai
+          </p>
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>fileupload</h1>
+            ) : (
+              <Link href="/sign-in">
+              <Button>Login to get Started!
+                <LogIn className="w-4 h-4 ml-2"></LogIn>
+              </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
